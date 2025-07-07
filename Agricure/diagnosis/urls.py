7c33c73c6f0fa18d5419507e087642bc35775fe6
Andrew <sseluyindaeva@gmail.com>
2025-07-07 17:14:48 +0300

    Initial commit

---
+++ Agricure/diagnosis/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'diagnosis'
+
+urlpatterns = [
+    # Diagnosis URLs will go here
+]
---
+++ Agricure/recommendations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'recommendations'
+
+urlpatterns = [
+    # Recommendation URLs will go here
+]
