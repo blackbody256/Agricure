e39c67b3a9c1ad951fbf3f67ac9a3a56a677eccd
Ayan Mustafa <ayanhilwa@gmail.com>
2025-07-16 14:21:52 +0300

    i have added a view dataset page for the agronomist

--- Agricure/agronomist/views.py
+++ Agricure/agronomist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from users.models import User  
+from administrator.models import DatasetUpload
+
 
 @login_required
 def dashboard(request):
@@ -8,3 +10,12 @@ def dashboard(request):
         return render(request, '403.html')  # or redirect with error
 
     return render(request, 'agronomist/dashboard.html')
+
+
+@login_required
+def dataset_list(request):
+    if request.user.role.lower() != 'agronomist':
+        return render(request, '403.html')  # Optional error page
+
+    datasets = DatasetUpload.objects.all().order_by('-uploaded_at')
+    return render(request, 'agronomist/dataset_list.html', {'datasets': datasets})
