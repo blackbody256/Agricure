9947eeafab484a27023d1ee4f268ee36ed43e392
Ayan Mustafa <ayanhilwa@gmail.com>
2025-07-17 12:52:51 +0300

    i have added the reports file to both the farmer and the Agronomist

--- Agricure/agronomist/urls.py
+++ Agricure/agronomist/urls.py
@@ -6,4 +6,5 @@ app_name = 'agronomist'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
      path('list/', views.dataset_list, name='dataset_list'),
+     path('report/', views.agronomist_report, name='report'),
 ]
